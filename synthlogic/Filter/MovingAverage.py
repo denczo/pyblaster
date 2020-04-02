b38a62a71852f1b3f5f4183e6ca12f07ac6b6b86
Dennis <d.czogalla@gmx.de>
2020-04-02 16:06:10 +0200

    clean up

--- synthlogic/Filter/MovingAverage.py
+++ synthlogic/Filter/MovingAverage.py
@@ -13,17 +13,3 @@ class MovingAverage:
     def output(self, x, M):
         kernel = np.ones(M)/M
         return np.convolve(x, kernel, 'same')
-
-    def average(self, x, M, pos):
-        size = len(x)
-        end = int(M/2) + pos
-        start = end - M
-
-        if end > size:
-            end = size
-        elif start < 0:
-            start = 0
-            #return np.sum(np.concatenate((self.delayLine[start:], x[:end]), axis=None))
-
-        return np.sum(x[start:end])/M
-
