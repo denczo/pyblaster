d1ac9124f1bffab0835eb2171771d19f6bc66b57
Dennis <d.czogalla@gmx.de>
2020-11-01 10:55:12 +0100

    test

--- synthlogic/main/synth.py
+++ synthlogic/main/synth.py
@@ -5,6 +5,8 @@ import numpy as np
 import threading
 import queue
 import sys
+
+import rtmidi
 from scipy.io import wavfile
 
 sys.path.append("/home/pi/synth")
@@ -13,6 +15,8 @@ from synthlogic.processing.envelope import Env
 from synthlogic.processing.filter import LowPass, Allpass
 import synthlogic.processing.oscillator as osc
 from synthlogic.structures.value import DataInterface
+
+midi_in = rtmidi.MidiIn()
 
 
 class Synth:
@@ -152,3 +156,53 @@ class Synth:
             #    self.toggle()
 
         # wavfile.write('recorded.wav', 44100, frames)
+
+
+
+def midi_info():
+    print("Available MIDI ports:\n")
+    available_ports = midi_in.get_ports()
+    for i in range(len(available_ports)):
+        print("[", i, "]", available_ports[i])
+    print("\nPlease select port of a MIDI device:")
+
+
+def run_synth_no_gui():
+    port = None
+    while not isinstance(port, int):
+        midi_info()
+        port = input()
+        try:
+            port = int(port)
+        except ValueError:
+            print("Port need to be a number!\n")
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    config.sections()
+    synth = Synth()
+    synth.change_midi_port(midi_in.get_port_name(port))
+    data = DataInterface()
+    synth.data_interface = data
+    synth.toggle()
+    print("OK")
+    # basic setup
+    synth.data_interface.harm_amount = int(config['HARM']['amount'])
+
+    # no logarithm needed
+    synth.data_interface.ft_cutoff.value = config['FILTER']['cutoff']
+    synth.data_interface.ft_reverb.value = config['FILTER']['reverb']
+
+
+    synth.data_interface.lfo_rate.value = config['LFO']['amount']
+    synth.data_interface.lfo_amount.value = config['LFO']['rate']
+    synth.data_interface.wf_type.value = config['OSC']['wf']
+
+    # checked, ok!
+    synth.data_interface.env_attack.value = config['ENV']['attack']
+    synth.data_interface.env_decay.value = config['ENV']['decay']
+    synth.data_interface.env_sustain.value = config['ENV']['sustain']
+    synth.data_interface.env_release.value = config['ENV']['release']
+    print("OK OK")
+
+run_synth_no_gui()
